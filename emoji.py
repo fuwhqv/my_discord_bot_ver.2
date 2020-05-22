1d22b0671b6f6f43152f83b74143163f3e1f5cbb
fuwhqv <dyk777@naver.com>
2020-05-22 11:32:50 +0900

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -6,16 +6,22 @@ emojiGroups = {}
 class emoji(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
+        await ctx.send('init0')
         for guild in self.bot.guilds:
+            await ctx.send('init1.g{}'.format(guild.name))
             for em in guild.emojis:
+                await ctx.send('init2.e{}'.format(em.name))
                 emojiGroups[em.name] = (em.animated, em.id)
 
 
     @commands.command()
     async def ereload(self, ctx):
         emojiGroups = {}
+        await ctx.send('erl0')
         for guild in self.bot.guilds:
+            await ctx.send('erl1.g{}'.format(guild.name))
             for em in guild.emojis:
+                await ctx.send('erl2.e{}'.format(em.name))
                 emojiGroups[em.name] = (em.animated, em.id)
         '''for x in emojiGroups.items():
             print(x)'''
@@ -42,10 +48,13 @@ class emoji(commands.Cog):
     async def elist(self, ctx):
         res = ''
         i=0
+        await ctx.send('el0')
         for name, em in list(emojiGroups.items()):
+            await ctx.send('el1.{}'.format(i))
             i+=1
             res += '<{0}:{1}:{2}> : `{1:15s}`{3}'\
                 .format('a' if em[0] else '', name, em[1], '\n' if i%3 == 0 else ' ')
+        await ctx.send('el2')
         await ctx.send(res)
 
 
