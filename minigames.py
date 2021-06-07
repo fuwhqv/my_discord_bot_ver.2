04e60097e11668481e91664d7674d8b67059b001
fuwhqv <dyk777@naver.com>
2021-06-07 14:36:03 +0900

    Update minigames.py

--- minigames.py
+++ minigames.py
@@ -5,13 +5,14 @@ from baseball import Baseball
 class minigames(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.game = None
+        self.game = {}
 
     @commands.command(name='play',
                       description='게임 진행을 위한 인풋/명령어 입력 커맨드',
                       aliases = ['input', 'move'])
     async def play(self, ctx, gCmd:str = None):
-        if self.game is None:
+        gGame = self.game.get(ctx.Guild)
+        if gGame is None:
             await ctx.send('No game playing!')
             return
         
@@ -19,7 +20,7 @@ class minigames(commands.Cog):
             await ctx.send('Invalid input!')
             return
 
-        result = self.game.update(gCmd)
+        result = gGame.update(gCmd)
         await ctx.send(result[1])
 
         if result[0] == 1:
@@ -28,14 +29,14 @@ class minigames(commands.Cog):
     @commands.command(name='숫자야구',
                       aliases=['baseball'])
     async def numberBaseball(self, ctx, digits:str ='3'):
-        if self.game is not None:
+        if self.game.get(ctx.Guild) is not None:
             await ctx.send('A game is already on play!')
             return
         try:
-            self.game = Baseball(int(digits))
+            self.game[ctx.Guild] = Baseball(int(digits))
             await ctx.send('The number is set, ready to play!')
         except:
-            self.game = None
+            self.game[ctx.Guild] = None
             await ctx.send('The number for digits is invalid!')
 
 
