ade810d27ed73d84512d87a3ac451f54e39da1fd
fuwhqv <dyk777@naver.com>
2021-10-31 19:34:37 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,16 +1,22 @@
 from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
+from random import choice
 
 class chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 
-    @commands.command()
+    @commands.command(name='ping')
     async def ping(self, ctx, n:int = 1):
         for _ in range(n):
             await ctx.send("Pong!")
 
+    @commands.command(name='sora'
+        ,alias=['소라고둥님']
+        )
+    async def sora(self, ctx, s = None):
+        await ctx.send('뭐' if s is None else choice(['응', '아니']))
 
     @commands.command(name='clear'
         ,description="최대 100개의 채팅 로그 삭제"
