49cf597afa1ac4aca8a88e7840605d0d0efcfd7e
fuwhqv <dyk777@naver.com>
2022-02-04 17:56:09 +0900

    Update minigames.py

--- minigames.py
+++ minigames.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from baseball import Baseball
-
+from wordle import Wordle
 
 class minigames(commands.Cog):
     def __init__(self, bot):
@@ -37,6 +37,18 @@ class minigames(commands.Cog):
         except:
             self.game[ctx.guild] = None
             await ctx.send('The number for digits is invalid!')
+
+    @commands.command(description='워들')
+    async def wordle(self, ctx, hard:str = 'Normal'):
+        if self.game.get(ctx.guild) is not None:
+            await ctx.send('A game is already on play!')
+            return
+        try:
+            self.game[ctx.guild] = Wordle(hard.upper() == 'HARD')
+            await ctx.send('The word is chosen, ready to play!')
+        except:
+            self.game[ctx.guild] = None
+            await ctx.send('Failed to create a wordle game...')
 
 
 def setup(bot):
